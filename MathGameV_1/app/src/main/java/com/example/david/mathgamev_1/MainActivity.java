2dc80726b616a3fb0201b40ccc58833d7e45a136
imnotarobot <j1376813@trbvm.com>
2016-01-01 20:17:44 +0100

    Alap

--- MathGameV_1/app/src/main/java/com/example/david/mathgamev_1/MainActivity.java
+++ MathGameV_1/app/src/main/java/com/example/david/mathgamev_1/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.david.mathgamev_1;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
+public class MainActivity extends Activity implements OnClickListener {
 
-public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +17,47 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button1:
+                intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                ;
+                break;
+            case R.id.button3:
+                intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button4:
+                intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button5:
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
     }
 }
